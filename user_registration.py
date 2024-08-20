63617cffd0b5a95114f867dac94b7b067358dffb
naveen <naiknaveen940@gmail.com>
2024-08-20 11:25:04 +0530

    [Naveen] add:user registration file for uc-3

--- user_registration.py
+++ user_registration.py
@@ -3,7 +3,7 @@
 @Date: 2024-08-16
 @Last Modified by: Naveen Madev Naik
 @Last Modified time: 2024-08-16
-@Title: Python program to check user entered first name and last name is valid
+@Title: Python program to check user entered first, last name and email is valid
 
 """
 
@@ -30,19 +30,39 @@ def validate_name(name: str):
     return re.search(pattern, name)
 
 
+def validate_email(email: str) :
+
+    """
+    Description:
+        Validates the email format according to given rule
+
+    Parameter:
+        email (str): The email to validate.
+
+    Return:
+        object
+    """
+
+    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z]+\.[a-zA-Z]{2,3}(\.[a-zA-Z]{2,3})?$'
+    return re.search(pattern, email)
+
+
 
 def main():
 
     first_name = input("Enter first name (first letter should be upper case): ")
     last_name=input("Enter Last name (first letter should be upper case )")
+    email=input("Enter email id:(eg:abc@gmail.com) ")
 
-    if validate_name(first_name) and validate_name(last_name):
-        logger.info("First name and Last name is valid")
+    if validate_name(first_name) and validate_name(last_name) and validate_email(email):
+        logger.info("First name ,Last name and Email is valid")
     else:
         if not validate_name(first_name):
             logger.info("First name is invalid")
         if not validate_name(last_name):
-            logger.info("Last name is invalid")    
+            logger.info("Last name is invalid")  
+        if not validate_email(email):  
+            logger.info("Email is invalid")    
 
 
 if __name__ == "__main__":
