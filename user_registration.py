e8dd00e4f5586f9a84d2d0a070994bafca1b6e50
naveen <naiknaveen940@gmail.com>
2024-08-20 11:49:39 +0530

    [Naveen] add:user registration file for uc-4

--- user_registration.py
+++ user_registration.py
@@ -3,7 +3,7 @@
 @Date: 2024-08-16
 @Last Modified by: Naveen Madev Naik
 @Last Modified time: 2024-08-16
-@Title: Python program to check user entered first, last name and email is valid
+@Title: Python program to check user entered first, last name, email and  is valid
 
 """
 
@@ -12,6 +12,7 @@ import re
 import mylogging
 
 logger=mylogging.logger_init("user_registration.py")
+
 
 def validate_name(name: str):
     
@@ -47,22 +48,41 @@ def validate_email(email: str) :
     return re.search(pattern, email)
 
 
+def validate_mobile_number(mobile_number: str):
+
+    """
+    Description:
+        Validates the mobile number format according to the given rule.
+
+    Parameter:
+        mobile_number (str): The mobile number to validate.
+
+    Return:
+        object
+    """
+
+    pattern = r'^\d{2} \d{10}$'
+    return re.search(pattern, mobile_number)
+
 
 def main():
 
     first_name = input("Enter first name (first letter should be upper case): ")
     last_name=input("Enter Last name (first letter should be upper case )")
     email=input("Enter email id:(eg:abc@gmail.com) ")
+    mobile_number=input("Enter mobile number(eg:91 8537865438) ")
 
-    if validate_name(first_name) and validate_name(last_name) and validate_email(email):
-        logger.info("First name ,Last name and Email is valid")
+    if validate_name(first_name) and validate_name(last_name) and validate_email(email) and validate_mobile_number(mobile_number):
+        logger.info("First Name ,Last Name, Email and Mobile Number is valid")
     else:
         if not validate_name(first_name):
             logger.info("First name is invalid")
         if not validate_name(last_name):
             logger.info("Last name is invalid")  
         if not validate_email(email):  
-            logger.info("Email is invalid")    
+            logger.info("Email is invalid")  
+        if not validate_mobile_number(mobile_number):
+            logger.info("Mobile number is invalid")      
 
 
 if __name__ == "__main__":
