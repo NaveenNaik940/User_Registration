17530f5b37befbebc372473aaf41b2b91c8c7354
naveen <naiknaveen940@gmail.com>
2024-08-20 12:02:11 +0530

    [Naveen] add:user registration test file for uc-5

--- user_registration_test.py
+++ user_registration_test.py
@@ -4,26 +4,26 @@
 @Date: 2024-08-16
 @Last Modified by: Naveen Madev Naik
 @Last Modified time: 2024-08-16
-@Title: Python program to validate first, last name, email and mobile number of user by unit testing
+@Title: Python program to validate first, last name, email,mobile number and password of user by unit testing
 
 """
 
 import unittest
-from user_registration import (validate_name,validate_email,validate_mobile_number)
+from user_registration import (validate_name,validate_email,validate_mobile_number,validate_password)
 
 class TestNameValidation(unittest.TestCase):
 
-    def test_case(self):
+    def test_first_last_name(self):
         
         """
         Description:
-            Checks if the first,last name,email and mobile number is valid according to the validation rule.
+            Checks if the first and last name is valid according to the validation rule.
 
         Parameter:
             self:instance of the class
 
         Return:
-            bool
+            None
         """
         
         #Test case validation for first name
@@ -38,11 +38,44 @@ class TestNameValidation(unittest.TestCase):
         self.assertFalse(validate_name("hjdj"))
         self.assertFalse(validate_name("isdh"))
 
+
+    def test_email(self): 
+        
+        """
+        Description:
+            Checks if email is valid according to the validation rule.
+
+        Parameter:
+            self:instance of the class
+
+        Return:
+            None
+        """
         #Test case validation for email
         self.assertTrue(validate_email("abc.xyz@bl.co.in"))
         self.assertTrue(validate_email("naveen@domain.co"))
         self.assertFalse(validate_email("naveen@blco"))
         self.assertFalse(validate_email("naveec@bl.co.in."))
+
+
+    def test_password(self):
+
+        """
+        Description:
+            Checks if password is valid according to the validation rule.
+
+        Parameter:
+            self:instance of the class
+
+        Return:
+            None
+        """        
+        #Test case validation for Password
+        self.assertTrue(validate_password("Naveen9087"))
+        self.assertTrue(validate_password("Nvaendhkl"))
+        self.assertFalse(validate_password("87"))
+        self.assertFalse(validate_password("aend"))
+        
 
         
 if __name__ == "__main__":
