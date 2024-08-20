8748228cdbb2779f72a51de6ec1e2a601c1ad601
naveen <naiknaveen940@gmail.com>
2024-08-20 11:15:16 +0530

    [Naveen] add:user registration file for uc-2

--- user_registration.py
+++ user_registration.py
@@ -3,7 +3,7 @@
 @Date: 2024-08-16
 @Last Modified by: Naveen Madev Naik
 @Last Modified time: 2024-08-16
-@Title: Python program to check user entered first name is valid
+@Title: Python program to check user entered first name and last name is valid
 
 """
 
@@ -13,32 +13,36 @@ import mylogging
 
 logger=mylogging.logger_init("user_registration.py")
 
-def validate_firstName(first_name: str):
+def validate_name(name: str):
     
     """
     Descripton: 
         Checks for the valid name
     
     Parameter: 
-        first_name:The user's name to validate.
+        name:The user's name to validate.
 
     return: 
         object
     """
     
     pattern = r'^[A-Z][A-Za-z]{2,}$'
-    return re.search(pattern, first_name)
+    return re.search(pattern, name)
 
 
 
 def main():
 
-    first_name = input("Enter first name first letter should be upper case: ")
+    first_name = input("Enter first name (first letter should be upper case): ")
+    last_name=input("Enter Last name (first letter should be upper case )")
 
-    if validate_firstName(first_name):
-        logger.info("First name is valid")
+    if validate_name(first_name) and validate_name(last_name):
+        logger.info("First name and Last name is valid")
     else:
-        logger.info("First name is invalid")
+        if not validate_name(first_name):
+            logger.info("First name is invalid")
+        if not validate_name(last_name):
+            logger.info("Last name is invalid")    
 
 
 if __name__ == "__main__":
